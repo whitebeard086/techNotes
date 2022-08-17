74df8da857809acec3618bb853b84e247cf61b8b
The Exorcist <whitebeard086@gmail.com>
2022-08-17 19:29:29 +0100

    Setting up CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,14 @@ const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
 
 // Middleware
 app.use(logger);
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
