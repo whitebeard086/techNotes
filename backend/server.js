4a976fb292ff60ef53a39ba63b6894c79c25e053
The Exorcist <whitebeard086@gmail.com>
2022-08-17 18:55:46 +0100

    Cookie parser middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3500;
 
 // Middleware
@@ -10,6 +11,8 @@ app.use(logger);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
