c7ece13356d1ec4a707b8d4dd3dd72aaa7ca3531
The Exorcist <whitebeard086@gmail.com>
2022-08-17 17:14:34 +0100

    Built-in middleware

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
 
