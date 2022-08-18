04bb7ca175c0d85575661c2168f6413a04c210fb
The Exorcist <whitebeard086@gmail.com>
2022-08-18 19:06:24 +0100

    Users controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const usersController = require("../controllers/usersController");
 
 router.route("/")
-    .get()
-    .post()
-    .patch()
-    .delete()
+    .get(usersController.getAllUsers)
+    .post(usersController.createNewUser)
+    .patch(usersController.updateUser)
+    .delete(usersController.deleteUser);
 
 module.exports = router;
