0c9fe097232fc396faf0ba11841e02087e816989
The Exorcist <whitebeard086@gmail.com>
2022-08-18 19:12:36 +0100

    get all users controller method

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -7,7 +7,13 @@ const bcrypt = require("bcrypt");
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
-    
+    const users = await User.find().select("-password").lean();
+
+    if (!users) {
+        return res.status(400).json({ message: "No users found" });
+    }
+
+    res.json(users);
 });
 
 // @desc Create a new user
