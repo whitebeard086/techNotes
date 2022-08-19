a408cdb7244f8cbfa8bc427b4d83751f0ccfef06
The Exorcist <whitebeard086@gmail.com>
2022-08-19 10:41:44 +0100

    Route note data requests

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcrypt");
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select("-password").lean();
 
-  if (!users) {
+  if (!users?.length) {
     return res.status(400).json({ message: "No users found" });
   }
 
@@ -106,8 +106,8 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 
   // Check if user has assigned notes
-  const notes = await Note.findOne({ user: id }).lean().exec();
-  if (notes?.length) {
+  const note = await Note.findOne({ user: id }).lean().exec();
+  if (note) {
     return res.status(400).json({ message: "User has assigned notes " });
   }
 
