92187b8fb1f016da0ceed2ec8fc0f9d77a560f98
The Exorcist <whitebeard086@gmail.com>
2022-08-19 11:04:21 +0100

    createNewNote controller method

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -23,6 +23,35 @@ const getAllNotes = asyncHandler(async (req, res) => {
   res.json(notesWithUser);
 });
 
+// @desc Create new note
+// @route POST /notes
+// @access Private
+const createNewNote = asyncHandler(async (req, res) => {
+  const { user, title, text } = req.body;
+
+  // Confirm data exists
+  if (!user || !title || !text) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // Check for duplicate title
+  const duplicate = await Note.findOne({ title }).lean().exec();
+
+  if (!duplicate) {
+    return res.status(409).json({ message: "Duplicate note title" });
+  }
+
+  // Create and store new note
+  const note = await Note.create({ user, title, text });
+
+  if (note) {
+    return res.status(201).json({ message: `New note ${title} created` });
+  } else {
+    return res.status(400).json({ message: "Invalid note data received" });
+  }
+});
+
 module.exports = {
   getAllNotes,
+  createNewNote,
 };
