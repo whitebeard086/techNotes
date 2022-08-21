606662bafa92d319eb9df857f87abdcea7da24fe
The Exorcist <whitebeard086@gmail.com>
2022-08-21 08:57:36 +0100

    Configure redux store

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const apiSliice = createApi({
+export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3500' }),
     tagTypes: ["Note", "User"],
     endpoints: builder => ({
-        
+
     })
 })
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 import "./index.css";
 import App from "./App";
@@ -8,10 +10,12 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
