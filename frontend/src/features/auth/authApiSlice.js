e3b6db9e0d2f22398af9c0450e3c0f9de1644481
The Exorcist <whitebeard086@gmail.com>
2022-09-07 22:14:24 +0100

    Fix for useQuery when it does not unsubscribe

--- frontend/src/features/auth/authApiSlice.js
+++ frontend/src/features/auth/authApiSlice.js
@@ -17,11 +17,12 @@ export const authApiSlice = apiSlice.injectEndpoints({
       }),
       async onQueryStarted(arg, { dispatch, queryFulfilled }) {
         try {
-          //const { data } =
-          await queryFulfilled;
-          //console.log(data)
+          const { data } = await queryFulfilled;
+          console.log(data);
           dispatch(logOut());
-          dispatch(apiSlice.util.resetApiState());
+          setTimeout(() => {
+            dispatch(apiSlice.util.resetApiState());
+          }, 1000);
         } catch (err) {
           console.log(err);
         }
