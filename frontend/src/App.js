0185a0f1b8cdb55ee415aeadbe591a6905312d5e
The Exorcist <whitebeard086@gmail.com>
2022-09-08 19:20:56 +0100

    Set credentials with onQueryStarted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import { DashLayout, Layout, Public } from "./components";
-import { EditNote, EditUser, Login, NewNote, NewUserForm, NotesList, Prefetch, UsersList, Welcome } from "./features";
+import { EditNote, EditUser, Login, NewNote, NewUserForm, NotesList, Prefetch, UsersList, Welcome, PersistLogin } from "./features";
 
 function App() {
   return (
@@ -10,25 +10,27 @@ function App() {
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
-        <Route element={<Prefetch />}>
-          <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<Prefetch />}>
+            <Route path="dash" element={<DashLayout />}>
+              <Route index element={<Welcome />} />
 
-            <Route path="users">
-              <Route index element={<UsersList />} />
-              <Route path=":id" element={<EditUser />} />
-              <Route path="new" element={<NewUserForm />} />
+              <Route path="users">
+                <Route index element={<UsersList />} />
+                <Route path=":id" element={<EditUser />} />
+                <Route path="new" element={<NewUserForm />} />
+              </Route>
+
+              <Route path="notes">
+                <Route index element={<NotesList />} />
+                <Route path=":id" element={<EditNote />} />
+                <Route path="new" element={<NewNote />} />
+              </Route>
+
             </Route>
-
-            <Route path="notes">
-              <Route index element={<NotesList />} />
-              <Route path=":id" element={<EditNote />} />
-              <Route path="new" element={<NewNote />} />
-            </Route>
-
           </Route>
-        </Route>
 
+        </Route>
       </Route>
     </Routes>
   );
